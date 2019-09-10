077bccb3c92fb4c077dfed1f961b2fa76711a3d0
Shea-Fyffe <shea.fyffe@gmail.com>
2019-09-09 23:06:45 -0400

    Shea Fyffe: Removed args from bayes_rule()...added Roxygen

--- R/bayesian_functions.R
+++ R/bayesian_functions.R
@@ -7,7 +7,7 @@
 #' @export
 #'
 #' @examples
-combination <- function(n,k) {
+combination <- function(n, k) {
    .res <- factorial(n) / (factorial(k) * factorial(n - k))
    return(.res)
 }
@@ -19,7 +19,7 @@ combination <- function(n,k) {
 #' @export
 #'
 #' @examples
-permutation <- function(n,k) {
+permutation <- function(n, k) {
    .res <- factorial(n) / factorial(n - k)
    return(.res)
 }
@@ -33,27 +33,31 @@ permutation <- function(n,k) {
 #' @examples
 full_house <- function(hand_size) {
    #2 triples 1 random card
-   .a <- combination(13,2) * combination(4,3) * combination(4,3) * combination(11,1) * combination(4,1)
+   .a <-
+      combination(13, 2) * combination(4, 3) * combination(4, 3) * combination(11, 1) * combination(4, 1)
    #1 triple 1 pair 2 random cards
-   .b <- combination(13,1) * combination(12,1) * combination(4,2) * combination(4,3) * combination(11,2) * combination(4,1)  * combination(4,1)
+   .b <-
+      combination(13, 1) * combination(12, 1) * combination(4, 2) * combination(4, 3) * combination(11, 2) * combination(4, 1)  * combination(4, 1)
    #1 triple 2 unique pair
-   .c <- combination(13,1) * combination(12,2) * combination(4,3) * combination(4,2) * combination(4,2)
+   .c <-
+      combination(13, 1) * combination(12, 2) * combination(4, 3) * combination(4, 2) * combination(4, 2)
    .res <- c(.a, .b, .c)
-   .res <- sapply(.res, function(x){
+   .res <- sapply(.res, function(x) {
       x <- x / combination(52, hand_size)
    })
    return(sum(.res))
 }
 #' @title Get Posterior Probabilities Based on Bayes Rule
 #'
-#' @param hand_size
+#' @param h Numeric. vector of length 2 P(H)
+#' @param d Numeric. vector of length 2 P(D|H)
+#' @param H1 Logical. Assume the Null Hypothesis is False?
 #'
 #' @return
 #' @export
 #'
 #' @examples
-bayes_rule <- function(h, d, H1 = TRUE,
-                       revise_cell, revise_value) {
+bayes_rule <- function(h, d, H1 = TRUE) {
    if (length(h) == 2L) {
       dh <- c(d, 1 - d)
    }
@@ -62,11 +66,11 @@ bayes_rule <- function(h, d, H1 = TRUE,
    }
    ct <- h * matrix(dh, ncol = 2)
    if (H1) {
-      pos <- ct[,1] / sum(ct[,1])
+      pos <- ct[, 1] / sum(ct[, 1])
    } else {
-      pos <- ct[,2] / sum(ct[,2])
+      pos <- ct[, 2] / sum(ct[, 2])
    }
-   ct <- cbind(h,d,ct,pos)
+   ct <- cbind(h, d, ct, pos)
    colnames(ct) <- c("Prior", "Likelihood", "BNH1", "BNH0", "POS")
    return(ct)
 }
