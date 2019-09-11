7d2282b73a51f36964b9b9c2f8ff679fdab58881
xsgab95 <50932393+xsgab95@users.noreply.github.com>
2019-09-11 12:39:54 -0400

    Update Project 2.R
    Correction

--- R/Project 2.R
+++ R/Project 2.R
@@ -10,7 +10,7 @@ train.dat$promotion <- train.dat$KPIs_met..80
 test.dat <- read.csv(file ="file:///C:/Users/gxesp/Documents/R/bayesian statistics/test_2umaH9m.csv", header = T, stringsAsFactors = T)
 test.dat$promotion <- test.dat$KPIs_met..80
 
-# Data frame filtered on Sales
+# Dataframe filtered on Sales
 filter.dat <- test.dat %>% filter(department == 'Sales & Marketing')
 
 # Contingency Table
@@ -39,10 +39,9 @@ print(con3)
 
 # Bayes function for frequency counts
 bayes <- function(prior_h1, con) {
-  lh <- (con[2, 2]/con[3, 2]) / (((con[2, 2]/con[3, 2]) * prior_h1) + ((con[2, 1]/con[3, 1]) * (1 - prior_h1)))
+  lh <- (con[2, 2]/con[3, 2]) / (((con[2, 2]/con[2, 3]) * prior_h1) + ((con[1, 2]/con[1, 3]) * (1 - prior_h1)))
   lh * prior_h1
 }
-
 
 # Initial prior probability from training data
 prior_promotion <- con1[3, 2] / con1[3, 3]
@@ -50,6 +49,7 @@ print(prior_promotion)
 
 # Update based on testing data
 bayes(prior_promotion, con2)
+
 # Update based on filtered sales data
 bayes(prior_promotion, con3)
 
