1ca9b1947ae5fda8cb37f3a972c8a1e6dc058399
aferna2 <54747017+aferna2@users.noreply.github.com>
2019-10-01 12:57:38 -0400

    Create Project 4_Beta.R

---
+++ R/Project 4_Beta.R
@@ -0,0 +1,42 @@
+# Project 4 mBayesian Estimation with Binomial Data
+# PS = prior success, PF = prior failure for beta dist. 
+# Default uniform distribution.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+# k = number of observed successes in the data, n = total trials
+
+a<-rbinom(10, 100, .6)
+plot(a)
+
+
+b<-dbinom(s bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb)
+
+library(tidyr)
+library(ggplot2)
+    
+bayes_beta <- function(PS = 1, PF = 1, k = 0, n = 0, title = "Beta Distributions"){
+  
+  # Generate prior, likelihood, and posterior distributions
+  y = seq(.001, .999, .001) 
+  
+  prior = dbeta(y, PS, PF)
+  likelihood = dbinom(rep(k, length(y)), n, y)
+  posterior = dbeta(y, PS + k, PF + n - k)
+  
+  # Standardize
+  prior = prior/sum(prior)
+  likelihood = likelihood/sum(likelihood)
+  posterior = posterior/sum(posterior)
+
+  # Use ggplot to graph data
+  dfBeta = data.frame(y, prior, likelihood, posterior)
+  dfBeta = gather(dfBeta, Distributions, Density, -y)
+  
+  dfBeta= dfBeta[dfBeta$Density > 0.001, ]
+    
+  p = ggplot(dfBeta, aes(y, Density, color = Distributions)) + geom_line() + labs(title = title)
+  p
+  
+}
+
+# Trump popular vote in 2016: 46.4%
+# Current Trump popularity poll from 538 Data: 43.2%
+bayes_beta(232, 268, 864, 2000, title = "Will Trump win the popular vote in 2020?")
