3cd493aaf794c1e898537d4f34dd435721a90fbe
xsgab95 <50932393+xsgab95@users.noreply.github.com>
2019-10-01 20:36:13 -0400

    Update Project 5.R
    Need to still add prior

--- R/Project 5.R
+++ R/Project 5.R
@@ -7,10 +7,10 @@ prosteph = read.csv(file ="file:///C:/Users/gxesp/Documents/R/bayesian statistic
 usteph = read.csv(file ="file:///C:/Users/gxesp/Documents/R/bayesian statistics/collegesteph.csv", header = T)
 
 # Find Prior Proportion
-PS = cumsum(usteph$X3P[1:3])
-PS = sum(PS)
-PF = cumsum(usteph$X3PA[1:3] - cumsum(usteph$X3P[1:3]))
-PF = sum(PF)
+PS = sum(usteph$X3P[1:3])
+PF = sum(usteph$X3PA[1:3]) - PS
+
+y = seq(.001, .999, .001)
 
 # Give a prior graph
 prior = dbeta(y, PS, PF)
@@ -26,15 +26,11 @@ Ml = matrix(data = 0, nrow = 999, ncol = 10)
 Mpos = matrix(data = 0, nrow = 999, ncol = 10)
 i = 0
 
-y = seq(.001, .999, .001)
 
 # Update 11 times, get posterior vector
 for (num in 1:10){
   
   i = i + 1
-  # Set prior: 
-  PS = 820
-  PF = 623
   
   # Generate likelihood, and posterior distributions
   likelihood = dbeta(y, 1 + k[i], 1 + n[i] - k[i])
@@ -51,12 +47,9 @@ for (num in 1:10){
   
 }
 
-# Convert matrices to dataframes (might need later?)
-likelihood_df = as_data_frame(Ml)
-posterior_df = as_data_frame(Mpos)
 
 # Basic Plot
-plot(y, type="n", ylim=c(0,max(posterior_df)), xlim=c(0, max(y)), ylab="Density")
+plot(y, type="n", ylim=c(0,max(Mpos)), xlim=c(0, max(y)), ylab="Density")
 colors = heat.colors(10)
 for(i in 1:10){
   t <- Mpos[, i]
@@ -71,3 +64,20 @@ title("Steph Curry's Predicted Performance Level")
 legend("topright", col = c("dark blue", "red"),lty = c(1, 1), 
        cex = 1, lwd = 1, bty = "n",
        legend = c("End of College", "Current Pro"))
+
+
+
+# Convert matrices to dataframes
+likelihood_df = as_data_frame(Ml)
+posterior_df = as_data_frame(Mpos)
+
+
+dfBeta = cbind(y, posterior_df)
+names(dfBeta)[-1] <- as.character(2009:2018)
+dfBeta = gather(dfBeta, Seasons, Density, -y)
+dfBeta= dfBeta[dfBeta$Density > 0.001, ]
+
+
+#ggplot
+p = ggplot(dfBeta, aes(y, Density, color = Seasons)) + geom_line() + labs(title = "Steph Curry's Predicted Performance Level")
+p
